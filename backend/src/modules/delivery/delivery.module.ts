308129229e3f0357ba4307d0c8abcb2b52cc8321
Suraj Kumar <suraj14879@gmail.com>
2026-07-19 22:07:22 +0530

    fix: add Store entity to DeliveryModule TypeOrmFeature to fix DI error

--- backend/src/modules/delivery/delivery.module.ts
+++ backend/src/modules/delivery/delivery.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DeliveryController } from './delivery.controller';
 import { DeliveryService } from './delivery.service';
-import { DeliveryTask, Order, User } from '../../database/entities';
+import { DeliveryTask, Order, User, Store } from '../../database/entities';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([DeliveryTask, Order, User])],
+  imports: [TypeOrmModule.forFeature([DeliveryTask, Order, User, Store])],
   controllers: [DeliveryController],
   providers: [DeliveryService],
   exports: [DeliveryService],
